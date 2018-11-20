b7506812bb7a374184a4a471ed3d54d2432f40e4
evengyl <dark.evengyl@gmail.com>
2018-11-20 20:20:50 +0100

    avance sur le jquery pour list left select

--- vues/search_pays_habitat.php
+++ vues/search_pays_habitat.php
@@ -2,7 +2,7 @@
 
  <div class="col-sm-6 col-md-4 col-lg-2 text-center">
     <br><br><br>
-    <div class="thumbnail">
+    <div class="thumbnail asset_left">
         <?
         if(isset($array_type[0]->icon_1))
             echo $array_type[0]->icon_1."&nbsp;";
@@ -23,11 +23,20 @@
         <div class="caption">
             <h4>Type de vacances : </h4>
             <p class="text-muted"><?= $array_type[0]->name; ?></p>
-            <p><a href="/recherche" class="btn btn-default" role="button">Je veux changer</a></p>
         </div>
+                    
+        
+        
     </div>
+    <p><a href="/recherche" class="btn btn-default" role="button">Je veux changer</a></p>
 </div>
 
+<!-- clone -->
+<div class="clone_part" style="display:none;">
+    <h4></h4>
+    <p class="text-muted"></p>
+</div> 
+<!-- end of clone -->
 
 <div class="container text-center">
     <div class="row">
@@ -48,7 +57,7 @@
                         <h3><?= $row_pays->name; ?></h3>
                         (1500 annonces)
                         <p class="text-muted"><?= $row_pays->text; ?></p>
-                        <button id="<?= $row_pays->url; ?>" data-id="<?= $row_pays->id; ?>" data-name="<?= $row_pays->name; ?>" class="btn btn-primary">Je sélectionne</button>
+                        <button id="<?= $row_pays->url; ?>" data-img="" data-id="<?= $row_pays->id; ?>" data-name="<?= $row_pays->name; ?>" class="inactive btn btn-primary">Je sélectionne</button>
                     </div>
                 </div>
             </div><?
@@ -57,7 +66,7 @@
 </div>
 
 
-<div class="container text-center" style="width:1600px;">
+<div class="container text-center">
     <div class="row">
         <h2 class="thin">Sélectionner à présent le ou les types de bien que vous chercher</h2>
         <p class="text-muted">
@@ -67,7 +76,7 @@
         <?  
             foreach($array_habitat as $row_habitat)
             {?>
-                <div class="col-sm-6 col-md-2">
+                <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
                         <i style="font-size:30px; color:#65b45199;" class="fas fa-home"></i>
                         <hr>
@@ -75,7 +84,7 @@
                         <div class="caption">
                             <h3><?= $row_habitat->name; ?></h3>(1200 annonces)
                             <p class="text-muted"><?= $row_habitat->text; ?></p>
-                            <button id="<?= $row_habitat->url; ?>" data-id="<?= $row_habitat->id; ?>" data-name="<?= $row_habitat->name; ?>" class="btn btn-primary">Je sélectionne</button>
+                            <button id="<?= $row_habitat->url; ?>" data-id="<?= $row_habitat->id; ?>" data-name="<?= $row_habitat->name; ?>" class="inactive btn btn-primary">Je sélectionne</button>
                         </div>
                     </div>
                 </div><?
@@ -84,7 +93,7 @@
 </div>
 
 
-<div class="container text-center" style="width:1600px;">
+<div class="container text-center">
     <div class="row">
         <p class="text-muted">
            Et voilà nous y somme déjà, vous n'avez plus qu'a valider et voir ce que l'on vous propose
@@ -99,8 +108,19 @@
     //le script suivant aura cette liste sur le coté comme POST donc ne pas oublier de générer le form a chauqe ajout de selections
     $(document).ready(function()
     {
-        $("button[data-id]").click(function(){
-            alert("ok click");
+        $("button.inactive[data-id]").click(function()
+        {
+
+            var img = '<img src="'+ $(this).parent().parent().find('img').attr('src') +'">';
+            var text = '<p class="text-muted">'+ $(this).attr("id") +'</p>';
+
+            console.log(img);
+
+            var clone_caption = $("div.clone_part").clone();
+            clone_caption.removeClass("clone_part").addClass("caption");
+            clone_caption.find("h4").text("Type de vacances : ").after(text).after(img);
+            $("div.asset_left").append(clone_caption);
+            $("div.asset_left div.caption").last().show();
         });
     });
 </script>
