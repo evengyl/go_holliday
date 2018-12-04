5664569c329d27c557e58420d6bbdb4be774c29c
lbaudoux <lbaudoux@matedex.be>
2018-12-04 14:38:27 +0100

    del img inutile + add logo ico

--- vues/top_head.php
+++ vues/top_head.php
@@ -8,8 +8,7 @@
 
 
 
-<link rel="shortcut icon" href="/images/gt_favicon.png">
-
+<link rel="shortcut icon" href="/images/logo.ico">
 <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 <link rel="stylesheet" href="/css/bootstrap.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
