55972918c1cb28ac26b25e83c891e58b1f11c3f8
lbaudoux <lbaudoux@matedex.be>
2019-01-11 15:00:23 +0100

    amenagement du sign up pays et formulaire en cours de validation dans le controller sign up

--- app/controller/sign_up.php
+++ app/controller/sign_up.php
@@ -32,63 +32,75 @@ Class sign_up extends base_module
 							->render_tpl();
 	}
 
+	private function verif_all_post($post)
+	{
+		foreach($post as $row_form_post)
+		{
+			if(empty($row_form_post))
+				return 0;
+		}
+		return 1;
+	}
+
 
 	public function treatment_sign_up($post)
 	{
-		affiche_pre($_POST);
-		/*
-	    if(isset($post["pseudo"]) && isset($post["password-1"]) && isset($post["password-2"]) && isset($post["email"]))
-	    {
-	    	$pseudo = $this->check_post_login($post['pseudo'], $is_pseudo = 1);
-	    	$password = $this->check_post_login($post['password-1']);
-	    	$password_verification = $this->check_post_login($post['password-2']);
-	    	$email = $this->check_post_login($post['email']);
+		$post = [
+			"login" => "legends",
+	    "mail" => "dark.evengyl@gmail.com",
+	    "last_name" => "baudoux",
+	    "name" => "loic",
+	    "age" => "27",
+	    "tel" => "0497312523",
+	    "genre" => "Monsieur",
+	    "address_rue" => "sous ghoys",
+	    "address_localite" => "labuissiere",
+	    "zip_code" => "6567",
+	    "pays" => "belgique",
+	    "rand_id_sign_up" => "1270243225"];
 
-	    	if(!$pseudo || !$password || !$password_verification || !$email)
-	    		$_SESSION['error_sign_up'] = "!! Attention votre login ou votre mot de passe est trop court !!";
+		affiche_pre($post);
 
-	    	else if($password != $password_verification)
-	    		$_SESSION['error_sign_up'] = "Les deux mots de passe ne correspondent pas.";
+		$post_verif = $this->verif_all_post($post);
+		if(!$post_verif)
+			$_SESSION['error_sign_up'] = "Le formulaire n'a bizarement pas été rempli correctement.";
 
-	    	else
-	    	{
+		else{
 
-	    		//check si le login existe déjà dans la bsd
+			//check si le login existe déjà dans la bsd
+    		$req_sql = new stdClass;
+			$req_sql->table = ["login"];
+			$req_sql->var = ["login"];
+			$req_sql->where = ["login = $1", [$post['login']]];
+			$res_sql = $this->_app->sql->select($req_sql);
+/*
+			//si le pseudo n'est pas existant on peux créer le login
+        	if(empty($res_sql))
+            {
+                $password_hash = password_hash($post["password"], PASSWORD_DEFAULT);
+
 	    		$req_sql = new stdClass;
-				$req_sql->table = ["login"];
-				$req_sql->var = ["login"];
-				$req_sql->where = ["login = $1", [$pseudo]];
-				$res_sql = $this->_app->sql->select($req_sql);
+				$req_sql->ctx = new stdClass;
+				$req_sql->ctx->login = $post["login"];
+				$req_sql->ctx->password_no_hash = $password;
+				$req_sql->ctx->password = $password_hash;
+				$req_sql->ctx->email = $email;
+				$req_sql->ctx->last_connect = date("U");
+				$req_sql->ctx->level = 0;
+				$req_sql->table = "login";
+				//$this->_app->sql->insert_into($req_sql);
 
-				//si le pseudo n'est pas existant on peux créer le login
-            	if(empty($res_sql))
-	            {
-	                $password_hash = password_hash($password, PASSWORD_DEFAULT);
-
-		    		$req_sql = new stdClass;
-					$req_sql->ctx = new stdClass;
-					$req_sql->ctx->login = $pseudo;
-					$req_sql->ctx->password_no_hash = $password;
-					$req_sql->ctx->password = $password_hash;
-					$req_sql->ctx->email = $email;
-					$req_sql->ctx->last_connect = date("U");
-					$req_sql->ctx->level = 0;
-					$req_sql->table = "login";
-					$this->_app->sql->insert_into($req_sql);
-
-					//va insrer les données de bases pour le commencent du jeu
-					
-					unset($_SESSION['error_sign_up']);
-					$_SESSION['error_sign_up'] = true;
-		            unset($_POST); //on vide le post
-		            return 1; //on
-		            
-	            }
-	            else
-	            	$_SESSION['error_sign_up'] = 'Ce pseudo est déjà utilisé, veuillez en choisir un autre, Merci.';
-	    	}	        
-	    }
-	    else
-	        $_SESSION['error_sign_up'] = 'Formulaire mal rempli';*/
+				//va insrer les données de bases pour le commencent du jeu
+				
+				unset($_SESSION['error_sign_up']);
+				$_SESSION['error_sign_up'] = true;
+	            unset($_POST); //on vide le post
+	            return 1; //on
+	            
+            }
+            else
+            	$_SESSION['error_sign_up'] = 'Ce pseudo est déjà utilisé, veuillez en choisir un autre, Merci.';
+*/
+		}
 	}
 }
--- vues/sign_up.php
+++ vues/sign_up.php
@@ -53,18 +53,39 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Login</span>
-								            	<input required id="login_form" name="login" type="text" value="" maxlength="20" pattern="[a-zA-Z_]{6,}" class="form-control" data-error="Votre login est trop court ou contient des chiffres" >
+								            	<input  id="login_form" name="login" type="text" maxlength="20" pattern="[a-zA-Z_]{6,}" class="form-control" data-error="Votre login est trop court ou contient des chiffres" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
 								    	</div>
 								    </div>
 
+
+							    	<div class="col-md-6">
+							            <div class="form-group has-feedback">
+							            	<div class="input-group">
+							            		<span class="input-group-addon">Mot de passe</span>
+								                <input name="password-new" type="password" data-minlength="6" class="form-control" id="inputPassword" data-error="Votre mot de passe doit contenir au minimum 6 caractères" placeholder="Mot de passe" >
+							                </div>
+							                <div class="help-block with-errors"></div>
+							            </div>
+							        </div>
+
+						            <div class="col-md-6">
+							            <div class="form-group has-feedback">
+							            	<div class="input-group">
+							            		<span class="input-group-addon">Confirmation</span>
+							                	<input type="password" class="form-control" id="inputPasswordConfirm" data-match="#inputPassword" data-match-error="Oups!, les deux mots de passe ne correspondent pas." placeholder="Confirmation du mot de passe" >
+							            	</div>
+							            </div>
+									</div>
+
+
 								    <div class="col-md-12">
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Adresse Email</span>
-								            	<input required id="mail_form" name="mail" type="email" value="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" class="form-control" data-error="Votre adresse Email n'est pas valide" >
+								            	<input  id="mail_form" name="mail" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" class="form-control" data-error="Votre adresse Email n'est pas valide" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -75,7 +96,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Nom</span>
-								            	<input required id="last_name_form" name="last_name" type="text" value="" maxlength="50" pattern="[^0-9]+" class="form-control" data-error="Caractère incorrect" >
+								            	<input  id="last_name_form" name="last_name" type="text" maxlength="50" pattern="[^0-9]+" class="form-control" data-error="Caractère incorrect" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -86,7 +107,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Prénom</span>
-								            	<input required id="name_form" name="name" type="text" value="" maxlength="50" pattern="[^0-9]+" class="form-control" data-error="Caractère incorrect" >
+								            	<input  id="name_form" name="name" type="text"  maxlength="50" pattern="[^0-9]+" class="form-control" data-error="Caractère incorrect" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -97,7 +118,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Age</span>
-								            	<input required id="age_form" name="age" type="text" value="" maxlength="3" pattern="[0-9]+" class="form-control" data-error="Max 999, uniquement des chiffres" >
+								            	<input  id="age_form" name="age" type="text" value="" maxlength="3" pattern="[0-9]+" class="form-control" data-error="Max 999, uniquement des chiffres" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -110,7 +131,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Téléphone (Mobile)</span>
-								            	<input required id="tel_form" name="tel" type="tel" value="" maxlength="10" pattern="[0-9]+" class="form-control" data-error="Caractère incorrect" >
+								            	<input  id="tel_form" name="tel" type="tel" value="" maxlength="10" pattern="[0-9]+" class="form-control" data-error="Caractère incorrect" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -123,25 +144,25 @@
 								                <span class="input-group-addon" style="border:none;">Genre</span>
 								                <div class="radio" style="display:inline-block; margin-right:10px;">
 								                    <label>
-								                        <input checked="checked" type="radio" name="genre" required>
+								                        <input value="Monsieur" checked="checked" type="radio" name="genre" >
 								                        Monsieur
 								                    </label>
 								                </div>
 								                <div class="radio" style="display:inline-block; margin-right:10px;">
 								                    <label>
-								                        <input type="radio" name="genre" required>
+								                        <input value="Madame" type="radio" name="genre" >
 								                        Madame
 								                    </label>
 								                </div>
 								                <div class="radio" style="display:inline-block; margin-right:10px;">
 								                    <label>
-								                        <input type="radio" name="genre" required>
+								                        <input value="Mademoiselle" type="radio" name="genre" >
 								                        Mademoiselle
 								                    </label>
 								                </div>
 								                <div class="radio" style="display:inline-block; margin-right:10px;">
 								                    <label>
-								                        <input type="radio" name="genre" required>
+								                        <input value="Ne se prononce pas" type="radio" name="genre" >
 								                        Ne se prononce pas
 								                    </label>
 								                </div>
@@ -156,7 +177,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Rue</span>
-								            	<input required id="address_rue_form" name="address_rue" type="text" value="" class="form-control">
+								            	<input  id="address_rue_form" name="address_rue" type="text" value="" class="form-control">
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -167,7 +188,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Numéro</span>
-								            	<input required id="address_numero_form" id="address_numero" type="text" value="" class="form-control" >
+								            	<input  id="address_numero_form" id="address_numero" type="text" value="" class="form-control" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -178,7 +199,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Ville</span>
-								            	<input required id="ville_form" name="address_localite" type="text" value="" class="form-control">
+								            	<input  id="ville_form" name="address_localite" type="text" value="" class="form-control">
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -189,7 +210,7 @@
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Code Postal</span>
-								            	<input required id="zip_code_form" name="zip_code" type="text" value="" maxlength="5" pattern="[0-9]+" class="form-control" data-error="Code postal incorrect" >
+								            	<input  id="zip_code_form" name="zip_code" type="text" value="" maxlength="5" pattern="[0-9]+" class="form-control" data-error="Code postal incorrect" >
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
@@ -197,37 +218,62 @@
 								    </div>
 
 								    <div class="col-md-12">
+								    	
 								    	<div class="form-group has-feedback">
 								        	<div class="input-group">
 								            	<span class="input-group-addon">Pays</span>
-								            	<input required id="pays_form" name="pays" type="text" value="" pattern="[a-zA-Z_\- ]+" class="form-control" data-error="Pays inconnu" >
+								            	<select name="pays" id="pays_form" class="form-control">
+								            		<option selected name="pays" value="Belgique">Belgique</option>
+								            		<option name="pays" value="France">France</option>
+								            		<option name="pays" value="French Guiana">French Guiana</option>
+								            		<option name="pays" value="French Polynesia">French Polynesia</option>
+								            		<option name="pays" value="French Southern Territories">French Southern Territories</option>
+								            		<option name="pays" value="Germany">Germany</option>
+								            		<option name="pays" value="Croatia">Croatia</option>
+								            		<option name="pays" value="Canada">Canada</option>
+								            		<option name="pays" value="Andorra">Andorra</option>
+								            		<option name="pays" value="Guadeloupe">Guadeloupe</option>
+								            		<option name="pays" value="Italie">Italie</option>
+								            		<option name="pays" value="Luxembourg">Luxembourg</option>
+								            		<option name="pays" value="Martinique">Martinique</option>
+								            		<option name="pays" value="Pays-Bas">Pays-Bas</option>
+								            		<option name="pays" value="Netherlands Antilles">Netherlands Antilles</option>
+								            		<option name="pays" value="Panama">Panama</option>
+								            		<option name="pays" value="Poland">Poland</option>
+								            		<option name="pays" value="Portugal">Portugal</option>
+								            		<option name="pays" value="Reunion Island">Reunion Island</option>
+								            		<option name="pays" value="Spain">Spain</option>
+								            		<option name="pays" value="United Kingdom">United Kingdom</option>
+								            		<option name="pays" value="United State">United State</option>
+								            	</select>
 								        	</div>
 								        	<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 								        	<div class="help-block with-errors"></div>
 								    	</div>
 								    </div>
-								</form>
 								
-								<p class="col-xs-12 text-center text-muted" style="margin-top:10px;">
-									Toutes les informations complémentaires serons modifiables dans le menu "Mon compte" puis "Profil"<hr>
-								</p>
-								<p class="col-xs-12 text-center text-muted" style="margin-top:10px;">
-									<b>Votre inscription sera validée par un administrateur, vous serez contacter par sms pour la véracité de votre demande.</b>
-								</p>
-								
+									
+									<p class="col-xs-12 text-center text-muted" style="margin-top:10px;">
+										Toutes les informations complémentaires serons modifiables dans le menu "Mon compte" puis "Profil"<hr>
+									</p>
+									<p class="col-xs-12 text-center text-muted" style="margin-top:10px;">
+										<b>Votre inscription sera validée par un administrateur, vous serez contacter par sms pour la véracité de votre demande.</b>
+									</p>
+									
 
-								<p class="text-center">
-									<a data-toggle="modal" data-target="#view_condition_general" class="opt_annonce">
-	                                	<small><i class="fa fa-angle-double-right "></i><span>&nbsp;Cliquer ici pour voir les conditions général du site&nbsp;</span><i class="fa fa-angle-double-left "></i></small>
-	                            	</a>
-                            	</p>
-								<hr>
+									<p class="col-xs-12 text-center">
+										<a data-toggle="modal" data-target="#view_condition_general" class="opt_annonce">
+		                                	<small><i class="fa fa-angle-double-right "></i><span>&nbsp;Cliquer ici pour voir les conditions général du site&nbsp;</span><i class="fa fa-angle-double-left "></i></small>
+		                            	</a>
+	                            	</p>
+									<hr>
 
-								<div class="text-center col-md-12"><hr>
-							    	<input type="hidden" name="rand_id_sign_up" value="<?= $rand_id_sign_up ?>">
-							        <button type="submit" class="btn btn-success btn-fill btn-wd">S'inscrire</button>
-							    </div>
-							    <div class="clearfix"></div>
+									<div class="text-center col-md-12"><hr>
+								    	<input type="hidden" name="rand_id_sign_up" value="<?= $rand_id_sign_up ?>">
+								        <button type="submit" class="btn btn-success btn-fill btn-wd">S'inscrire</button>
+								    </div>
+								    <div class="clearfix"></div>
+							    </form>
 								
 							</div>
 						</div>
@@ -261,3 +307,6 @@
         </div>
     </div>
 </div>
+
+
+
