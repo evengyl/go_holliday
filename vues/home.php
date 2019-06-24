3b76de806cd3cd948cddd34081e502e7b0e0ab5f
lbaudoux <lbaudoux@matedex.be>
2019-06-24 11:00:22 +0200

    avance sur le new style

--- vues/home.php
+++ vues/home.php
@@ -2,25 +2,27 @@
 	<div class="container">
 		<div class="row">
 			<h1 class="col-12 lead">Bienvenue sur <?= $_app->site_name; ?></h1>
-			<p class="col-12 tagline"><a style="background: #0007; padding: 4px;">&nbsp;&nbsp;&nbsp;Votre site de recherche gratuit pour trouver vos vacances&nbsp;&nbsp;&nbsp;</a></p>
-			<p class="col-12"><a class="btn btn-default btn-lg" href="/Recherche" style="color:white; background:#0006;" role="button">Chercher ce dont vous rêvez</a></p><?
+			<p class="col-12 tagline"><a style="background: #0008; padding: 4px;">&nbsp;&nbsp;&nbsp;Votre site de recherche gratuit pour trouver vos vacances&nbsp;&nbsp;&nbsp;</a></p>
+			<p class="col-12"><a class="btn btn-success" href="/Recherche" role="button">Chercher ce dont vous rêvez</a></p><?
 			if($_app->option_app['app_with_login_option'])
 			{
 				if(!Config::$is_connect)
-					echo '<p><a class="btn btn-action btn-lg" href="/Inscription" role="button">Vous inscrire</a></p>';
+					echo '<p class="col-12"><a class="btn btn-warning" href="/Inscription" role="button">Vous inscrire</a></p>';
 			}?>
 		</div>
 	</div>
 </header>
 
-<div class="container text-center">
-	<br><br>
-	<h2 class="thin">Nous réalisons vos envies de vacances simple et directement en ligne</h2>
-	<p class="text-muted">
-		Nous vous proposons une interface simple et intuitive pour chercher / trouver / réservez vos vacances en familles ou en couples.</br>
-		Vous aurez accès a une interface gratuite et sans limite d'utilisation pour gérer vos vacances car :<br> nous vous mettons directement en relation avec un propriétaire 
-		de bungalow, appartement, maison d'hôtes, maison de vacances.
-	</p>
+<div class="container">
+	<div class="text-center">
+		<br><br>
+		<h2 class="thin ">Nous réalisons vos envies de vacances simple et directement en ligne</h2>
+		<p class="text-muted text-center">
+			Nous vous proposons une interface simple et intuitive pour chercher / trouver / réservez vos vacances en familles ou en couples.</br>
+			Vous aurez accès a une interface gratuite et sans limite d'utilisation pour gérer vos vacances car :<br> nous vous mettons directement en relation avec un propriétaire 
+			de bungalow, appartement, maison d'hôtes, maison de vacances.
+		</p>
+	</div>
 </div>
 
 __MOD2_search_type__
--- vues/nav_top.php
+++ vues/nav_top.php
@@ -1,56 +1,55 @@
-<div class="navbar navbar-inverse navbar-fixed-top headroom" >
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+<div class="navbar navbar-fixed-top navbar-expand-lg">
+    <a class="navbar-brand col align-self-end" href="/Accueil">
+        <img style="display:inline;" src="/images/logo.png" alt="">
+        <span style="display: inline; margin-top:5px;" class="thin"><?= $_app->site_name; ?></span><br>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav_bar_top" aria-controls="nav_bar_top" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-            <a class="navbar-brand" href="/Accueil">
-                <img style="display:inline;" src="/images/logo.png" alt="">
-                <span style="display: inline; margin-top:5px;" class="thin"><?= $_app->site_name; ?></span><br>
-            </a>
-        </div>
+    <div class="collapse navbar-collapse" id="nav_bar_top">
+        <ul class="navbar-nav col align-self-end">
 
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav pull-right">
-                <li class=""><form><input type="text" style="margin-top:8px;" class="form-control" name="search" placeholder="Recherche"></form></li>
-                <li class="<?=($_GET['page'] == 'home')?'active':'' ?>"><a href="/Accueil">__TRANS_accueil__</a></li>
-                <li class="<?=($_GET['page'] == 'recherche')?'active':'' ?>"><a href="/Recherche">Recherche de vacances</a></li>
-                <li class="<?=($_GET['page'] == 'contact')?'active':'' ?>"><a href="/Contact">__TRANS_contact_us__</a></li><?
 
-                if($_app->option_app['app_with_login_option'])
+            <li class="nav-item <?=($_GET['page'] == 'home')?'active':'' ?>"><a class="nav-link" href="/Accueil">__TRANS_accueil__</a></li>
+            <li class="nav-item <?=($_GET['page'] == 'recherche')?'active':'' ?>"><a class="nav-link" href="/Recherche">Recherche de vacances</a></li>
+            <li class="nav-item <?=($_GET['page'] == 'contact')?'active':'' ?>"><a class="nav-link" href="/Contact">__TRANS_contact_us__</a></li><?
+
+            if($_app->option_app['app_with_login_option'])
+            {
+                if(Config::$is_connect)
                 {
-                    if(Config::$is_connect)
-                    {
-                        $active = "";
-                        if($_GET['page'] == 'my_account')
-                            $active = "active";
-                        echo '<li class="' . $active . '"><a href="/Mon_compte">Mon Compte</a></li>';
-                    }
-
-                    if(!Config::$is_connect)
-                    {
-                        $active = "";
-                        if($_GET['page'] == 'login')
-                            $active = "active";
-                        echo '<li class="' . $active . '"><a class="btn" href="/Connexion">Se connecter</a></li>';
-                    }
-
-                    if(Config::$is_connect)
-                    {
-                        $active = "";
-                        if($_GET['page'] == 'logout')
-                            $active = "active";
-                        echo '<li class="' . $active . '"><a class="btn" href="/Deconnection">Se déconnecter</a></li>';
-                    }
-
-                    
+                    $active = "";
+                    if($_GET['page'] == 'my_account')
+                        $active = "active";
+                    echo '<li class="nav-item ' . $active . '"><a class="nav-link" href="/Mon_compte">Mon Compte</a></li>';
                 }
-                ?>
-            </ul>
-        </div>
-        __MOD2_breadcrumb__
+
+                if(!Config::$is_connect)
+                {
+                    $active = "";
+                    if($_GET['page'] == 'login')
+                        $active = "active";
+                    echo '<li class="nav-item ' . $active . '"><a class="nav-link" class="btn" href="/Connexion">Se connecter</a></li>';
+                }
+
+                if(Config::$is_connect)
+                {
+                    $active = "";
+                    if($_GET['page'] == 'logout')
+                        $active = "active";
+                    echo '<li class="nav-item ' . $active . '"><a class="nav-link" class="btn" href="/Deconnection">Se déconnecter</a></li>';
+                }
+
+                
+            }
+            ?>
+        </ul>
+        <form class="form-inline col align-self-end">
+            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Recherche">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
+        </form>
     </div>
+        __MOD2_breadcrumb__
 </div> 
+
